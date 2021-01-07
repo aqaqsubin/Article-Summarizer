f0ff53b5fcfbfad7db28615f185c7078d206fab0
subin Kim <subinkim@iMac-2.local>
2021-01-07 18:06:27 +0900

    Add crawling module

---
+++ crawlNaverNews.py
@@ -0,0 +1,107 @@
+# _*_ coding: utf-8 _*_
+
+import requests
+import os
+from bs4 import BeautifulSoup
+from loadNews import get_text_without_children
+
+NAVER_NEWS_BASE_URL = "news.naver.com"
+NAVER_SEARCH_URL = "https://search.naver.com/search.naver"
+NAVER_SEARCH_POLICE_URL = "https://search.naver.com/search.naver?&where=news&query=경찰"
+
+USER_AGENT = 'Mozilla/5.0'
+
+SQL_FILE_PATH = 'insertSqlQuery.sql'
+SQL_INSERT_BASE_QUERY = "INSERT INTO {table_name} (title, media, url) VALUES (%s, %s, %s);"
+
+
+def get_append_option(filename):
+    if os.path.exists(filename):
+        append_write = 'a'  # append if already exists
+    else:
+        append_write = 'w'  # make a new file if not
+    return append_write
+
+
+def get_search_page_list():
+
+    search_page_list = []
+    url = NAVER_SEARCH_POLICE_URL
+
+    while True:
+    # for i in range(10):
+        try:
+            search_response = requests.get(url, headers={'User-Agent': USER_AGENT})
+            search_html = BeautifulSoup(search_response.text, "html.parser")
+
+            next_page_url = search_html.select_one("#main_pack > div.api_sc_page_wrap > div > a.btn_next")["href"]
+            print(i, next_page_url)
+            search_page_list.append(NAVER_SEARCH_URL + next_page_url)
+
+            url = search_page_list[-1]
+
+        except Exception as e:
+            print("Error", e)
+            break;
+
+    return search_page_list;
+
+
+def parse_article_info(article):
+    title = article.select_one("a.news_tit")["title"]
+
+    divs = article.find_all('a', {"class": "info press"})
+    media = get_text_without_children(divs[0])
+
+    src_addr = article.select_one("div.news_info > div.info_group > a:last-of-type")["href"]
+
+    return title, media, src_addr
+
+
+def insert_to_db():
+    os.system("psql newsdb < " + SQL_FILE_PATH)
+    os.remove(SQL_FILE_PATH)
+
+def convert_txt(text): # Convert Single Quote -> Double Quote, Wrapping Text with Single Quote
+    return "\'"+text.replace("\'","\"")+"\'"
+
+def save_article_info(article_info, flag=False):
+    if not NAVER_NEWS_BASE_URL in article_info['src_addr']: return
+
+    baseQuery = "INSERT INTO news_list (title, media, url) VALUES ({title}, {media}, {url});"
+    query = baseQuery.format(title=article_info['title'], media=article_info['media'],
+                             url=article_info['src_addr'])
+
+    f = open(SQL_FILE_PATH, get_append_option(SQL_FILE_PATH))
+    f.write(query + "\n")
+    f.close()
+
+    fileStat = os.stat(SQL_FILE_PATH)
+    fileSize = fileStat.st_size
+
+    if fileSize > 100 * 1000: insert_to_db()
+    elif flag : insert_to_db()
+
+
+def get_article_list():
+
+    for page_no, page_url in enumerate(get_search_page_list()):
+
+        page_response = requests.get(page_url, headers={'User-Agent': USER_AGENT})
+        page_html = BeautifulSoup(page_response.text, "html.parser")
+
+        article_area_list = page_html.select("ul.list_news > li.bx ")
+        for idx, article_area in enumerate(article_area_list):
+            article = article_area_list[idx].select_one("div.news_wrap.api_ani_send > div.news_area")
+            title, media, src_addr = parse_article_info(article)
+
+            article_info = {'title': convert_txt(title),
+                            'media': convert_txt(media),
+                            'src_addr': convert_txt(src_addr)}
+
+            save_article_info(article_info)
+
+    save_article_info(article_info, flag=True)
+
+if __name__ == "__main__":
+    get_article_list()
---
+++ loadNews.py
@@ -0,0 +1,84 @@
+# _*_ coding: utf-8 _*_
+
+import psycopg2 as pg2
+import requests
+import os
+from bs4 import BeautifulSoup
+from shutil import rmtree
+
+USER_AGENT = 'Mozilla/5.0'
+CONTENT_DIR = "./articles"
+
+
+def mkdir_p(path):
+    import errno
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
+def del_folder(path):
+    try:
+        rmtree(path)
+    except:
+        pass
+
+def get_article_list_fromDB():
+    conn = None
+    article_list = []
+    try:
+        conn = pg2.connect(host='localhost', dbname='newsdb', user='subinkim', password='123', port='5432')  # db에 접속
+        cur = conn.cursor()
+
+        cur.execute("SELECT title, url FROM news_list;")
+        rows = cur.fetchall()
+        for row in rows:
+            article_list.append(dict(title=row[0], url=row[1]))
+
+    except (Exception, pg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+    return article_list
+
+
+def get_text_without_children(tag):
+    return ''.join(tag.find_all(text=True, recursive=False)).strip()
+
+
+def parse_article_content(article_html, tag, id):
+    content = ''
+    divs = article_html.find_all(tag, {"id": id})
+
+    for i in divs:
+        content += get_text_without_children(i)
+
+    return content
+
+
+def save_article_content_txt(article_no, content):
+    f = open(os.path.join(CONTENT_DIR, str(article_no) + ".txt"), 'w')
+    f.write(content + '\n')
+    f.close()
+
+
+def get_article_content():
+    article_list = get_article_list_fromDB()
+
+    for article_no, article in enumerate(article_list):
+        article_page_response = requests.get(article['url'], headers={'User-Agent': USER_AGENT})
+        article_html = BeautifulSoup(article_page_response.text, "html.parser")
+
+        content = parse_article_content(article_html, 'div', 'articleBodyContents')
+        save_article_content_txt(article_no, content)
+
+
+if __name__ == "__main__":
+    mkdir_p(CONTENT_DIR)
+    get_article_content()
+
