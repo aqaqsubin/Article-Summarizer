39fe0afe77db4e6f24026fa35f7fdf4283f2b6e0
aqaqsubin <aqaq925120k@naver.com>
2021-01-09 10:54:27 +0900

    Add Article Info to text file

--- loadNews.py
+++ loadNews.py
@@ -34,10 +34,10 @@ def get_article_list_fromDB():
         conn = pg2.connect(host='localhost', dbname='newsdb', user='subinkim', password='123', port='5432')  # db에 접속
         cur = conn.cursor()
 
-        cur.execute("SELECT title, url FROM news_list;")
+        cur.execute("SELECT title, url, media FROM news_list;")
         rows = cur.fetchall()
         for row in rows:
-            article_list.append(dict(title=row[0], url=row[1]))
+            article_list.append(dict(title=row[0], url=row[1], media=row[2]))
 
     except (Exception, pg2.DatabaseError) as error:
         print(error)
@@ -66,10 +66,11 @@ def parse_article_content(article_html, tag, id):
     return content
 
 
-def save_article_content_txt(article_no, title, content):
+def save_article_content_txt(article_no, title, content, media):
     f = open(os.path.join(CONTENT_DIR, str(article_no) + ".txt"), 'w', -1, "utf-8")
     f.write(title + '\n')
     f.write(content + '\n')
+    f.write(media + '\n')
     f.close()
 
 
@@ -84,7 +85,7 @@ def get_article_content():
         print(url['content'])
         if NAVER_NEWS_BASE_URL in url['content'] :
             content = parse_article_content(article_html, 'div', 'articleBodyContents')
-            save_article_content_txt(article_no, article['title'], content)
+            save_article_content_txt(article_no, article['title'], content, article['media'])
 
 if __name__ == "__main__":
     mkdir_p(CONTENT_DIR)
